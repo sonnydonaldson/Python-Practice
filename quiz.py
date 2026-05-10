834caf70c6e61a125de4faed886092c866ffd8a6
sonnydonaldson <donaldsons@wsc.school.nz>
2026-05-11 11:59:18 +1200

    starting the quiz and making a question

---
+++ quiz.py
@@ -0,0 +1,10 @@
+# introduction to the quiz and getting there name 
+INTRODUCTION = "This is a quiz about random stuff hope you like it"
+player_name = input("What is your name ").capitalize()
+print("hello " + player_name)
+print(INTRODUCTION)
+
+
+# First question and getting there answer for it 
+
+first_question_answer = input("")
