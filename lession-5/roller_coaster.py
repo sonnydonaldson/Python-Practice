6924235bdeaa2ab4d4498ca38f689ca3c255b2f1
sonnydonaldson <donaldsons@wsc.school.nz>
2026-05-28 10:24:56 +1200

    doing the extension for the rollercoster

--- lession-5/roller_coaster.py
+++ lession-5/roller_coaster.py
@@ -23,44 +23,89 @@
 # Follow the same task (with extension), but use dictionaries to make the code more efficient
 
 #getting there name
+
 name = input("Hello what is your name ").capitalize()
 print(f"hello {name}")
 
 
-#getting there hight and age
-height = input("how tall are you in centimeters ")
-height = int(height)
-age = input("what is your age ")
-age = int(age)
 
 
-#seeing if the have a heart condtion
+#seeing what rollercoster they want to go on
+
+
 answer_vaild = False
 while answer_vaild == False:
-    heart_condition = input("Do you have a heart condition ")
-
-    if heart_condition.upper().strip() == "YES":
-        heart_condition = True
-        answer_vaild =  True
-    elif heart_condition.upper().strip() == "NO": 
-        heart_condition = False
-        answer_vaild = True
-
-#seeing if the have a vip pass
-answer_vaild = False
-while answer_vaild == False:
-    vip_pass = input("Do you have a VIP pass ")
-    if vip_pass.upper().strip( ) == "YES":
-        vip_pass = True
-        answer_vaild = True
-    elif vip_pass.upper().strip() == "NO": 
-        vip_pass = False
-        answer_vaild = True
-   
+        #getting there hight and age
+    height = input("how tall are you in centimeters ")
+    height = int(height)
+    age = input("what is your age ")
+    age = int(age)
 
 
+    #seeing if the have a heart condtion
+    answer_vaild = False
+    while answer_vaild == False:
+        heart_condition = input("Do you have a heart condition ")
 
-if height > 150 and age > 10 and heart_condition == False or vip_pass == True : 
-    print(f"You are allowed on the ride, go ahead {name}")
-elif not height > 150 or age > 10 or heart_condition == True :
-    print(f"You are not allowed on the ride {name}")
+        if heart_condition.upper().strip() == "YES":
+            heart_condition = True
+            answer_vaild =  True
+        elif heart_condition.upper().strip() == "NO": 
+            heart_condition = False
+            answer_vaild = True
+
+    #seeing if the have a vip pass
+    answer_vaild = False
+    while answer_vaild == False:
+        vip_pass = input("Do you have a VIP pass ")
+        if vip_pass.upper().strip( ) == "YES":
+            vip_pass = True
+            answer_vaild = True
+        elif vip_pass.upper().strip() == "NO": 
+            vip_pass = False
+            answer_vaild = True
+        
+
+        # seeing which rollercoster they want to go on 
+        print("There are four roller costers to chose from\n pride before\n orphan maker\n I laughed as you fell\n normal boring rollercoster")
+        which_rollercost = input("which one do you want to go on ").strip().upper()
+
+        #telling them which rollercoster they can go on 
+        if which_rollercost == "NORMAL BORING ROLLERCOSTER" and height > 150 and age > 10 and heart_condition == False or which_rollercost == "NORMAL BORING ROLLERCOSTER" and vip_pass == True : 
+            print(f"You are allowed on the ride, go ahead {name}")
+            answer_vaild == True
+        elif which_rollercost == "NORMAL BORING ROLLERCOSTER" and not height > 150 or age > 10 or heart_condition == True :
+            print(f"You are not allowed on the ride {name}")
+            answer_vaild == True
+        elif which_rollercost == "ORPHAN MAKER" and height > 180 and age > 31 and heart_condition == False or which_rollercost == "ORPHAN MAKER" and vip_pass == True:
+            print(f"You are allowed on the ride, go ahead {name}")
+            answer_vaild == True
+        elif which_rollercost == "ORPHAN MAKER" and not height > 180 or age > 31 or heart_condition == True :
+            print(f"You are not allowed on the ride {name}")
+            answer_vaild == True
+        elif which_rollercost == "I LAUGHED AS YOU FELL" and height > 50 and age > 3 and heart_condition == False  or which_rollercost == "I LAUGHED AS YOU FELL" and vip_pass == True:
+            print(f"You are allowed on the ride, go ahead {name}")
+            answer_vaild == True
+        elif  which_rollercost == "I LAUGHED AS YOU FELL" and not height > 50 or age > 3 or heart_condition == True:
+            print(f"You are not allowed on the ride {name}")
+            answer_vaild == True
+        elif answer_vaild == "PRIDE BEFORE" and height > 200 and age > 90 and heart_condition == False or answer_vaild == "PRIDE BEFORE" and vip_pass == True:
+            print(f"You are allowed on the ride, go ahead {name}")
+            answer_vaild == True
+        elif  answer_vaild == "PRIDE BEFORE" and not height > 200 or age > 90 or heart_condition == True:
+             print(f"You are not allowed on the ride {name}")
+             answer_vaild == True
+
+
+
+
+
+
+
+
+    """
+    if height > 150 and age > 10 and heart_condition == False or vip_pass == True : 
+        print(f"You are allowed on the ride, go ahead {name}")
+    elif not height > 150 or age > 10 or heart_condition == True :
+        print(f"You are not allowed on the ride {name}")
+    """
--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+#varible to make sure they can't mess up and break the code
+answer_vaild = False
 # introduction to the quiz and getting there name 
 INTRODUCTION = "This is a quiz about random stuff hope you like it"
 player_name = input("What is your name ").capitalize()
@@ -6,31 +8,32 @@ print(f"{INTRODUCTION} \n")
 
 
 # First question and getting their answer for it 
+
 print(f"First question(this one is multiple choice)")
 print("What is the fastest any man made craft has gone(so it has a person in side of it)\n A = 37,824 km/h \n B = 11.1km/s \n C = 383,947 Mph \n D = 3,692 mph")
-first_question_answer = input()
+first_question_answer = input().upper().strip()
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if first_question_answer in ("b", "B"):
+if first_question_answer == "B":
     print("Correct that is the answer it was achieved by the crew of Apollo 10 \n")
 
-if first_question_answer not in ("b", "B"):
+elif first_question_answer not in ("B"):
     print("Incorrect the answer is 11.1km/s or  24,791mph or 39,897km/h this was done by the crew of apollo \n")
 
 
 
 # second question and getting their answer for it
 print("Second question")
-second_question_answer = input("What element has the highest melting point")
+second_question_answer = input("What element has the highest melting point").upper().strip()
 
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if second_question_answer in ("Tungsten", "tungsten"): 
+if second_question_answer in ("TUNGSTEN"): 
     print("Correct it has a melting point of 3,422 celises \n")
 
-if second_question_answer not in ("Tungsten", "tungsten"):
+elif second_question_answer not in ("TUNGSTEN"):
     print("Incorrect the element is Tungsten with a melting point of 3422 celises\n")
 
 
@@ -38,15 +41,15 @@ if second_question_answer not in ("Tungsten", "tungsten"):
 
 print("Third question(this one is multiple choice)")
 print("Who created the theory of relativity\n A = Isaac Newton \n B = Galileo Galilei \n C = Albert Einstein \n D = Nikola Tesla")
-third_question_answer = input()
+third_question_answer = input().upper().strip()
 
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if third_question_answer in ("C", "c"):
+if third_question_answer == "C":
     print("Correct it Was Albert Einstein")
 
-if third_question_answer not in ("C", "c"):
+elif third_question_answer not in ("C"):
     print("Incorrect the answer is C, Albert Einstein")
     
 
@@ -54,29 +57,29 @@ if third_question_answer not in ("C", "c"):
 
 print("fourth question(this one is multiple choice)")
 print("How big was Tsar Bomba originally going to be(in megatons)\n A = 25 magatons B = 58 magatons C = 90 magatons D = 100 magaton E = 50 magatons")
-fourth_question_answer = input()
+fourth_question_answer = input().upper().strip()
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
 
-if fourth_question_answer in ("D", "d"):
+if fourth_question_answer in ("D"):
     print("Correct the reason it was smaller was because of concerns over massive radioactive fallout and the high risk of not allowing the bomber crew enough time to escape the explosion.")
 
-if fourth_question_answer not in ("D", "d"):
+elif fourth_question_answer not in ("D"):
     print("Incorrect the answer is 100 magaton the reason it was smaller was because there was concerns over massive radioactive fallout and the high risk of not allowing the bomber crew enough time to escape the explosion.")
 
 
 # fifth question and getting their answer for it 
 
 print("Fifth question(true or false )")
-fifth_question_answer = input("eletrons are the smallest subatomic particle")
+fifth_question_answer = input("eletrons are the smallest subatomic particle").upper().strip()
 
 #cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if fifth_question_answer in ("True", "true"):
+if fifth_question_answer in ("TRUE"):
     print("correct the smallest subatomic particle is Quarks")
 
-if fifth_question_answer not in ("True", "true"):
+elif fifth_question_answer not in ("TRUE"):
     print("Incorrect the smallest subatomic particle is Quarks")
 
 
