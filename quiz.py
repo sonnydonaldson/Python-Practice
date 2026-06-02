a69c336824b7b4e5c0d87214d9d2a8b1a14060a5
sonnydonaldson <donaldsons@wsc.school.nz>
2026-06-02 15:05:03 +1200

    adding a scoring system and worked on my feedback

--- quiz.py
+++ quiz.py
@@ -1,96 +1,100 @@
 #varible to make sure they can't mess up and break the code
 answer_vaild = False
+score = 0 
 # introduction to the quiz and getting there name 
-INTRODUCTION = "This is a quiz about random stuff hope you like it"
-player_name = input("What is your name ").capitalize()
+INTRODUCTION = "This is a quiz about random stuff hope you like it and if it is a multpile choice answer in the upper case letters like A B C D. you will also be scored with you getting plues one with every question correct"
+player_name = input("What is your name ").capitalize().strip()
 print(f"hello {player_name}.")
 print(f"{INTRODUCTION} \n")
 
 
 # First question and getting their answer for it 
 
-print(f"First question(this one is multiple choice)")
+print(f"\n\nFirst question(this one is multiple choice)")
 print("What is the fastest any man made craft has gone(so it has a person in side of it)\n A = 37,824 km/h \n B = 11.1km/s \n C = 383,947 Mph \n D = 3,692 mph")
-first_question_answer = input().upper().strip()
+question_answer = input().upper().strip()
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if first_question_answer == "B":
+if question_answer in ("B", "11.1KM/S"):
     print("Correct that is the answer it was achieved by the crew of Apollo 10 \n")
-
-elif first_question_answer not in ("B"):
+    score +=1  
+else:
     print("Incorrect the answer is 11.1km/s or  24,791mph or 39,897km/h this was done by the crew of apollo \n")
 
 
 
 # second question and getting their answer for it
-print("Second question")
-second_question_answer = input("What element has the highest melting point").upper().strip()
+print("\n\nSecond question")
+question_answer = input("What element has the highest melting point ").upper().strip()
 
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if second_question_answer in ("TUNGSTEN"): 
+if question_answer in ("TUNGSTEN"): 
     print("Correct it has a melting point of 3,422 celises \n")
-
-elif second_question_answer not in ("TUNGSTEN"):
+    score +=1  
+else:
     print("Incorrect the element is Tungsten with a melting point of 3422 celises\n")
 
 
 # third question and getting their answer for it
 
-print("Third question(this one is multiple choice)")
+print("\n\nThird question(this one is multiple choice)")
 print("Who created the theory of relativity\n A = Isaac Newton \n B = Galileo Galilei \n C = Albert Einstein \n D = Nikola Tesla")
-third_question_answer = input().upper().strip()
+question_answer = input().upper().strip()
 
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if third_question_answer == "C":
+if question_answer in ("C", "ALBERT EINSTEIN" ):
     print("Correct it Was Albert Einstein")
-
-elif third_question_answer not in ("C"):
+    score +=1  
+else:
     print("Incorrect the answer is C, Albert Einstein")
     
 
 # fourth question and getting their answer for it 
 
-print("fourth question(this one is multiple choice)")
-print("How big was Tsar Bomba originally going to be(in megatons)\n A = 25 magatons B = 58 magatons C = 90 magatons D = 100 magaton E = 50 magatons")
-fourth_question_answer = input().upper().strip()
+print("\n\nfourth question(this one is multiple choice)")
+print("How big was Tsar Bomba originally going to be(in megatons)\n A = 25 magatons B = 58 magatons C = 90 magatons D = 100 magatons E = 50 magatons")
+question_answer = input().upper().strip()
 
 # cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
 
-if fourth_question_answer in ("D"):
+if question_answer in ("D", "100 MAGATONS", "100"):
     print("Correct the reason it was smaller was because of concerns over massive radioactive fallout and the high risk of not allowing the bomber crew enough time to escape the explosion.")
-
-elif fourth_question_answer not in ("D"):
+    score +=1  
+else:
     print("Incorrect the answer is 100 magaton the reason it was smaller was because there was concerns over massive radioactive fallout and the high risk of not allowing the bomber crew enough time to escape the explosion.")
 
 
 # fifth question and getting their answer for it 
 
-print("Fifth question(true or false )")
-fifth_question_answer = input("eletrons are the smallest subatomic particle").upper().strip()
+print("\n\nFifth question(true or false )")
+question_answer = input("eletrons are the smallest subatomic particle ").upper().strip()
 
 #cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if fifth_question_answer in ("TRUE"):
+if question_answer in ("FALSE", "QUARKAS "):
     print("correct the smallest subatomic particle is Quarks")
-
-elif fifth_question_answer not in ("TRUE"):
+    score +=1  
+else:
     print("Incorrect the smallest subatomic particle is Quarks")
 
 #sixth question and getting their answer for it 
 
-print("Sixth Question")
-sixth_question_answer = input("What does sound travel faster in air or water").upper().strip()
+print("\n\nSixth Question")
+question_answer = input("What does sound travel faster in air or water ").upper().strip()
 
 
 #cheaking their answer is correct and printing the repily for if it is correct or incorrect
 
-if sixth_question_answer in ("WATER"):
+if question_answer in ("WATER"):
     print("Correct sound travels faster in water")
-elif sixth_question_answer not in ("WATER"):
+    score +=1  
+else:
     print("Incorrect sound travels faster in water")
+
+print(f"your final score was {score}")
