bca71d94c63446128f88eb63faa4b3ebe25d086c
sonnydonaldson <donaldsons@wsc.school.nz>
2026-06-05 13:41:19 +1200

    rosey teaching me more things about coding

---
+++ avandace coding with rosey  /pratice.py
@@ -0,0 +1,12 @@
+name = input("hello what is your name ").upper().strip()
+
+def makesomething():
+    if name != "SONNY":
+        return "good"
+    else:
+        return "bad"
+answer = makesomething()
+
+print(f"{answer}")
+
+
