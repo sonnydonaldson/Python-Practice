a451780b8129f836d21cdc34f3ac9c2b88bfce96
sonnydonaldson <donaldsons@wsc.school.nz>
2026-07-02 10:25:28 +1200

    class inside of class and kerbooard work out

--- avandace coding with rosey  /pratice.py
+++ avandace coding with rosey  /pratice.py
@@ -1,8 +1,18 @@
+import random
+import keyboard
 
+eRelease = True
+while True:
+    if keyboard.is_pressed("e") and eRelease:
+        print("Yellow")
+        eRelease = False
+
+    elif not keyboard.is_pressed("e"):
+        eRelease = True
 """
 name = input("hello what is your name ").upper().strip()
 
-def makesomething():
+def makesomething():  
     if name != "SONNY":
         return "good"
     else:
@@ -12,6 +22,7 @@ answer = makesomething()
 print(f"{answer}")
 
 """
+"""
 class person():
     def __init__(self,iframe,hframe):
         self.iframe = iframe
@@ -20,3 +31,35 @@ class person():
 
 sonny = person("hi", "sonny")
 print(sonny.iframe + sonny.hframe)
+
+
+
+class room():
+    def __init__(self, name):
+        self.name = name
+
+    def describeRoom(self):
+        print(f"{self.name} - A dusty old room")
+
+class combatRoom(room):
+    def __init__(self, name, enemy):
+        super().__init__(name)
+        self.enemy = enemy
+
+    def describeRoom(self):
+        print(f"{self.name} - Contains {self.enemy}")
+
+class shopRoom(room):
+    def __init__(self, name, forSale):
+        super().__init__(name)
+        self.forSale = forSale
+
+    def item(self):
+        print(f"The item {self.forSale} is for sale")
+
+rooms = [
+    room("Cabin"),
+    combatRoom("Dungeon", "Vampire"),
+    shopRoom("Shop", "Sword")
+]
+"""
